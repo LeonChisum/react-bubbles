8155e45dbddcf9c102f3e5f32e93c981b125742a
Leon Chisum <leonchisum3306@gmail.com>
2020-01-25 14:14:23 -0500

    Login form rendered, token received

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,12 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
+import { Form } from 'semantic-ui-react'
+import axios from 'axios'
 
-const Login = () => {
+const Login = (props) => {
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
+  const [user, setUser] = useState({
+    username: 'Lambda School',
+    password: 'i<3Lambd4'
+  })
+
+  const handleChange = (e) => {
+    setUser({
+        ...user,
+        [e.target.name] : e.target.value
+    })
+}
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    console.log(user)
+    axios.post(`http://localhost:5000/api/login`, user)
+          .then(res => {
+            console.log(res)
+            const token = res.data.payload
+            localStorage.setItem('token', token)
+            props.history.push('/home')
+          })
+          .catch(err => console.log(err))
+  }
+
   return (
     <>
       <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
+      <Form onSubmit={onSubmit}>
+          <Form.Input 
+            fluid label='Username' 
+            placeholder='Username'
+            name='username'
+            value={user.username}
+            onChange={handleChange} />
+          <Form.Input 
+            fluid label='Password' 
+            placeholder='Enter Password'
+            name='password'
+            value={user.password}
+            onChange={handleChange} />
+          <Form.Button>Login</Form.Button>
+      </Form>
     </>
   );
 };
